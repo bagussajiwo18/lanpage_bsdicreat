e0e61bad6c9108d5e4095918565f567c8e0c3034
bagussajiwo18 <bagussajiwo18@gmail.com>
2026-08-19 19:14:22 +0700

    update layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,7 +11,10 @@ const plusJakartaSans = Plus_Jakarta_Sans({
 export const metadata: Metadata = {
   title: 'BS DICREAT - Jasa Desain Grafis Kreatif',
   description: 'BS DICREAT adalah jasa desain grafis profesional untuk visual branding, media sosial, dan media promosi. Solusi kreatif dan elegan untuk membuat bisnis Anda tampil beda dan tepercaya.',
-  keywords: ['bs dicreat', 'jasa desain', 'jasa desain grafis', 'jasa desain grafis cilegon', 'jasa desain digital'],
+  keywords: ['bs dicreat', 'bsdicreat', 'jasa desain', 'jasa desain grafis', 'jasa desain grafis cilegon', 'jasa desain grafis banten', 'pembuatan banner promosi'],
+  verification: {
+    google: '<meta name="google-site-verification" content="FbN0TtOeDHStgoQlAQDGyzxQL58c5He1Icz1U-I7-LU" />',
+  },
   openGraph: {
     title: 'BS DICREAT - Jasa Desain Grafis Kreatif & Berkualitas',
     description: 'BS DICREAT adalah jasa desain grafis profesional untuk visual branding, media sosial, dan media promosi. Solusi kreatif dan elegan untuk membuat bisnis Anda tampil beda dan tepercaya.',
