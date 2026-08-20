73a4c1a238ddc3d471a74d88a061a4f4ef852bed
bagussajiwo18 <bagussajiwo18@gmail.com>
2026-08-20 21:21:41 +0700

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,22 +10,22 @@ const plusJakartaSans = Plus_Jakarta_Sans({
 
 export const metadata: Metadata = {
   title: 'BS DICREAT - Jasa Desain Grafis Kreatif',
-  description: 'BS DICREAT adalah jasa desain grafis profesional untuk visual branding, media sosial, dan media promosi. Solusi kreatif dan elegan untuk membuat bisnis Anda tampil beda dan tepercaya.',
-  keywords: ['bs dicreat', 'bsdicreat', 'jasa desain', 'jasa desain grafis', 'jasa desain grafis cilegon', 'jasa desain grafis banten', 'pembuatan banner promosi'],
+  description: 'BS DICREAT, yang merupakan singkatan dari BS Digital Creative, adalah penyedia jasa desain grafis profesional untuk visual branding, media sosial, dan media promosi. Solusi kreatif dan elegan untuk membuat bisnis Anda tampil beda dan tepercaya.',
+  keywords: ['bs dicreat', 'bsdicreat', 'bs digital creative', 'jasa desain', 'jasa desain grafis', 'jasa desain grafis cilegon', 'jasa desain grafis banten', 'pembuatan banner promosi'],
   verification: {
     google: 'FbN0TtOeDHStgoQlAQDGyzxQL58c5He1Icz1U-I7-LU',
   },
   openGraph: {
-    title: 'BS DICREAT - Jasa Desain Grafis Kreatif & Berkualitas',
-    description: 'BS DICREAT adalah jasa desain grafis profesional untuk visual branding, media sosial, dan media promosi. Solusi kreatif dan elegan untuk membuat bisnis Anda tampil beda dan tepercaya.',
+    title: 'BS DICREAT - Jasa Desain Grafis Kreatif',
+    description: 'BS DICREAT, yang merupakan singkatan dari BS Digital Creative, adalah penyedia jasa desain grafis profesional untuk visual branding, media sosial, dan media promosi. Solusi kreatif dan elegan untuk membuat bisnis Anda tampil beda dan tepercaya.',
     url: 'https://bsdicreat.vercel.app',
-    siteName: 'BS DICREAT',
+    siteName: 'BS DICREAT (BS Digital Creative)',
     images: [
       {
         url: '/images/letter_bsdicreat.png',
         width: 1200,
         height: 675,
-        alt: 'BS DICREAT Lettermark',
+        alt: 'BS DICREAT (BS Digital Creative) Lettermark',
       },
     ],
     locale: 'id_ID',
